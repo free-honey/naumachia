5bbe7769faf7f9c04782f9f85aec6f33b554c9f2
MitchTurner <james.mitchell.turner@gmail.com>
2022-12-09 20:06:12 -0800

    Use json file as input

--- sample-dApps/always-succeeds-contract/src/logic/script.rs
+++ sample-dApps/always-succeeds-contract/src/logic/script.rs
@@ -2,10 +2,12 @@ use naumachia::scripts::raw_script::PlutusScriptFile;
 use naumachia::scripts::raw_validator_script::RawPlutusValidator;
 use naumachia::scripts::{ScriptError, ScriptResult};
 
-const SCRIPT_CBOR: &str = include_str!("../../always_succeeds/assets/always_true/spend/script.txt");
+const SCRIPT_RAW: &str =
+    include_str!("../../always_succeeds/assets/always_true/spend/payment_script.json");
 
 pub fn get_script() -> ScriptResult<RawPlutusValidator<(), ()>> {
-    let script_file = PlutusScriptFile::new("AikenV2", "should always succeed", SCRIPT_CBOR);
+    let script_file: PlutusScriptFile = serde_json::from_str(SCRIPT_RAW)
+        .map_err(|e| ScriptError::FailedToConstruct(e.to_string()))?;
     let raw_script_validator = RawPlutusValidator::new_v2(script_file)
         .map_err(|e| ScriptError::FailedToConstruct(e.to_string()))?;
     Ok(raw_script_validator)
