5660afdf08216537dd11c47cc0f52c92ffbc2075
MitchTurner <james.mitchell.turner@gmail.com>
2023-02-25 17:19:51 -0800

    Create happy-path version

--- sample-dApps/checking_account/src/scripts/pull_validator.rs
+++ sample-dApps/checking_account/src/scripts/pull_validator.rs
@@ -26,10 +26,85 @@ pub fn spend_token_policy() -> ScriptResult<RawPlutusValidator<CheckingAccountDa
 mod tests {
     use super::*;
     use crate::{Address, CheckingAccountDatums};
-    use naumachia::scripts::context::{pub_key_hash_from_address_if_available, ContextBuilder};
+    use naumachia::scripts::context::{
+        pub_key_hash_from_address_if_available, ContextBuilder, PubKeyHash, TxContext,
+    };
     use naumachia::scripts::ValidatorCode;
 
     const NETWORK: u8 = 0;
+
+    struct TestContext {
+        pub signer_pkh: PubKeyHash,
+        pub range_lower: Option<(i64, bool)>,
+        pub range_upper: Option<(i64, bool)>,
+
+        pub input_address: Address,
+
+        pub input_tx_id: Vec<u8>,
+        pub input_index: u64,
+        pub input_token_policy_id: String,
+        pub input_datum: Option<CheckingAccountDatums>,
+
+        pub output_address: Address,
+        pub output_token_policy_id: String,
+        pub output_datum: Option<CheckingAccountDatums>,
+    }
+
+    impl TestContext {
+        pub fn happy_path() -> Self {
+            let signer = Address::from_bech32("addr_test1qrksjmprvgcedgdt6rhg40590vr6exdzdc2hm5wc6pyl9ymkyskmqs55usm57gflrumk9kd63f3ty6r0l2tdfwfm28qs0rurdr").unwrap();
+            let signer_pkh = pub_key_hash_from_address_if_available(&signer).unwrap();
+            let script = spend_token_policy().unwrap();
+            let input_tx_id = [8, 8, 8, 8];
+            let input_tx_index = 0;
+            let script_address = script.address(NETWORK).unwrap();
+            let spending_token = vec![5, 5, 5, 5];
+            let input_datum = CheckingAccountDatums::AllowedPuller {
+                next_pull: 0,
+                period: 10,
+                spending_token: spending_token.clone(),
+            };
+            let policy_id = hex::encode(&spending_token);
+            let output_datum = CheckingAccountDatums::AllowedPuller {
+                next_pull: 10,
+                period: 10,
+                spending_token,
+            };
+            TestContext {
+                signer_pkh,
+                range_lower: Some((11, true)),
+                range_upper: None,
+                input_address: script_address.clone(),
+                input_tx_id: input_tx_id.to_vec(),
+                input_index: input_tx_index,
+                input_token_policy_id: policy_id.clone(),
+                input_datum: Some(input_datum),
+                output_address: script_address,
+                output_token_policy_id: policy_id.clone(),
+                output_datum: Some(output_datum),
+            }
+        }
+
+        pub fn build(&self) -> TxContext {
+            let mut input_builder = ContextBuilder::new(self.signer_pkh.clone())
+                .with_range(self.range_lower.clone(), self.range_upper.clone())
+                .with_input(&self.input_tx_id, self.input_index, &self.input_address)
+                .with_value(&self.input_token_policy_id, "something", 1);
+            if let Some(input_datum) = &self.input_datum {
+                input_builder = input_builder.with_inline_datum(input_datum.clone())
+            }
+            let mut output_builder = input_builder
+                .finish_input()
+                .with_output(&self.output_address)
+                .with_value(&self.output_token_policy_id, "something", 1);
+            if let Some(output_datum) = &self.output_datum {
+                output_builder = output_builder.with_inline_datum(output_datum.clone())
+            }
+            output_builder
+                .finish_output()
+                .build_spend(&self.input_tx_id, self.input_index)
+        }
+    }
 
     #[test]
     fn execute__after_next_pull_date_succeeds() {
@@ -173,36 +248,10 @@ mod tests {
 
     #[test]
     fn execute__happy_path() {
-        let signer = Address::from_bech32("addr_test1qrksjmprvgcedgdt6rhg40590vr6exdzdc2hm5wc6pyl9ymkyskmqs55usm57gflrumk9kd63f3ty6r0l2tdfwfm28qs0rurdr").unwrap();
-        let signer_pkh = pub_key_hash_from_address_if_available(&signer).unwrap();
+        let ctx_builder = TestContext::happy_path();
+        let input_datum = ctx_builder.input_datum.clone().unwrap();
         let script = spend_token_policy().unwrap();
-        let input_tx_id = [8, 8, 8, 8];
-        let input_tx_index = 0;
-        let script_address = script.address(NETWORK).unwrap();
-        let spending_token = vec![5, 5, 5, 5];
-        let input_datum = CheckingAccountDatums::AllowedPuller {
-            next_pull: 0,
-            period: 10,
-            spending_token: spending_token.clone(),
-        };
-        let policy_id = hex::encode(&spending_token);
-        let output_datum = CheckingAccountDatums::AllowedPuller {
-            next_pull: 10,
-            period: 10,
-            spending_token,
-        };
-        let ctx = ContextBuilder::new(signer_pkh)
-            .with_range(Some((11, true)), None)
-            .with_input(&input_tx_id, input_tx_index, &script_address)
-            .with_value(&policy_id, "something", 1)
-            .with_inline_datum(input_datum.clone())
-            .finish_input()
-            .with_output(&script_address)
-            .with_value(&policy_id, "something", 1)
-            .with_inline_datum(output_datum)
-            .finish_output()
-            .build_spend(&input_tx_id, input_tx_index);
-
+        let ctx = ctx_builder.build();
         let _eval = script.execute(input_datum, (), ctx).unwrap();
     }
 
